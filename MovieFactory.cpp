16458f9cada1b70748333945f1203b737f18aa1b
Tammy <taamyhle@uw.edu>
2022-03-01 11:59:38 -0800

    Added to MovieFactory Class: .cpp Implemented constructors and create methods
    Default constructor throwing errors, fix later

--- MovieFactory.cpp
+++ MovieFactory.cpp
@@ -6,21 +6,46 @@
  * In MovieFactory we're creating the movie objects
  * This means we will be creating and returning a new movie object
  * All factory classes are like Hashmaps
+ * comedy: F, Stock, Director, title, yearReleased
+ * drama: D, Stock, Director, title, yearReleased
+ * classics: C, Stock, Director, title, majorActor, releaseDate
  * @date 2022-02-26
  **/
 
 #include "Movie.h"
 
-// default constructor
+// default constructor - need to declare elements in hashmap and in movieFactory
 MovieFactory::MovieFactory()
 {
+    movieFac.push_back(new Comedy);
+    movieFac.push_back(new Drama);
+    movieFac.push_back(new Classics);
+    
+    //Down below is throwing errors
+    //movieMap.insert({'F', 10});
+    //movieMap.insert({'D', 10});
+    //movieMap.insert({'C', 10});
 }
 // destructor - deletes movie objects from vector 
 MovieFactory::~MovieFactory()
 {
+    for (int i = 0; i < movieFac.size(); i++)
+    {
+        delete movieFac[i];
+        movieFac[i] = nullptr;
+    }
 }
 // creating the movie object, overrides Item's create function
 // override is optional
 Item* MovieFactory::create(char movieType) const 
 {
+    if (movieMap.find(movieType) == movieMap.end())
+    {
+        return nullptr;
+    }
+    else
+    {
+        int subscript = movieMap.at(movieType);
+        return movieFac[subscript]->create();
+    }
 }
