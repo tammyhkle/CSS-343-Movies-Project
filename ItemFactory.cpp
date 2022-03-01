affab3f87d46a2f6245ee0acf57f535e4ff901ec
Tammy <taamyhle@uw.edu>
2022-03-01 11:18:25 -0800

    Added to ItemFactory Class (.h) Implemented .cpp: Constructors

---
+++ ItemFactory.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file ItemFactory.cpp
+ * @author Le, Tammy
+ * @brief ItemFactory class - ItemFactory is the class where we create Item objects
+ * Notes:
+ * When the ItemFactory is called on, it creates an Item object 
+ * @date 2022-02-26
+ **/
+
+#include "ItemFactory.h"
+
+// default constructor
+ItemFactory::ItemFactory()
+{};
+ // destructor - deletes ItemFactory object
+ItemFactory::~ItemFactory() 
+{};
--- ItemFactory.h
+++ ItemFactory.h
@@ -1,23 +1,28 @@
-/*
-  ItemFactory is the class where we create Item objects
-*/
+/**
+ * @file ItemFactory.h
+ * @author Le, Tammy
+ * @brief ItemFactory class - ItemFactory is the class where we create Item objects
+ * Notes:
+ * When the ItemFactory is called on, it creates an Item object 
+ * @date 2022-02-26
+ **/
 
 #ifndef ITEMFACTORY_H
 #define ITEMFACTORY_H
-
-#include <map>
-#include <vector>
-#include <string>
 
 #include "BSTree.h"
 #include "Item.h"
 #include "Movie.h"
 
+#include <map>
+#include <vector>
+#include <string>
+
 using namespace std;
 
 class ItemFactory {
 public:
-  ItemFactory(); // constructor
+  ItemFactory(); // default constructor
   virtual ~ItemFactory(); // destructor
   virtual Item *create(char prefix) const = 0; // where we create the item, using a char prefix that's been read and     passed in. We keep it virtual since it's going to be overriden.
 };
