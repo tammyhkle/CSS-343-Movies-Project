80d0f346f9ad0b639758dacc845c7ccbea18197c
Tammy <taamyhle@uw.edu>
2022-03-01 23:16:11 -0800

    Modified MovieFactory: .h Cleaned header file (comments)

--- MovieFactory.h
+++ MovieFactory.h
@@ -26,10 +26,8 @@ class MovieFactory: public ItemFactory {
 public:
   MovieFactory(); // default constructor
   virtual ~MovieFactory(); // destructor - deletes movie objects from vector 
+  Item *create(char movieType) const override; // creating the movie object, overrides Item's create function; override is optional
 
-  Item *create(char movieType) const override; // creating the movie object, overrides Item's create function
-  // override is optional
-    
 private:
   unordered_map<char, int> movieMap; // Hashmap with keys equal to char variables of every possible movie genre and values equal to integers ranging from 0 to the number of move types -1
   vector<Movie*> movieFac; // vector with elements that are initialized as new movie objects of every possible movie type
